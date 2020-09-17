027fa4dd9d313fb6c7a14a4305391d0e936d0897
Guru <guru@travelcreed.com>
2020-09-17 10:15:58 -0700

    init: Angular 7 application setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'exercise-senior-front-end';
+}
